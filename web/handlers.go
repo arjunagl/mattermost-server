4745777d416a4c618e60bb2557ff8ff91e1f60fe
Arjuna Marambe <arjunam@buildxact.com>
2020-11-11 13:58:31 +1100

    work in progress

--- web/handlers.go
+++ web/handlers.go
@@ -202,6 +202,7 @@ func (h Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		} else if !session.IsOAuth && tokenLocation == app.TokenLocationQueryString {
 			c.Err = model.NewAppError("ServeHTTP", "api.context.token_provided.app_error", nil, "token="+token, http.StatusUnauthorized)
 		} else {
+			fmt.Println("Setting1")
 			c.App.SetSession(session)
 		}
 
